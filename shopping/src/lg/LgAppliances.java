fcb3f67b7f027846000b3626f23753f3d96d52b2
Ali <Ali@DESKTOP-O5I7VTC>
2019-05-19 23:41:43 +0530

    3rd

--- shopping/src/lg/LgAppliances.java
+++ shopping/src/lg/LgAppliances.java
@@ -1,13 +1,9 @@
 package lg;
 
-import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 
