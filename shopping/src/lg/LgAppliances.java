035400cd973634bd7a8cdd9511b00511e674bf5c
Ali <Ali@DESKTOP-O5I7VTC>
2019-05-19 23:46:50 +0530

    comited

--- shopping/src/lg/LgAppliances.java
+++ shopping/src/lg/LgAppliances.java
@@ -2,31 +2,10 @@ package lg;
 
 
 
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.Test;
 
 
 public class LgAppliances {
-	WebDriver driver;
-
-
-
-	@Test()
-	public void login(String id, String pwd, String pom, String tom) {
-			System.out.println(System.getProperty("user.dir"));
-
-	}
-
 
 	
-	@AfterTest
-	public void finish() {
-		System.out.println("dono hogaye cange");
-		//
-		//
-		
-
-	}
 
 }
